571b7372a682cc986c5a4c5a9d790a014f0687b6
Neto <97475687+neto2022@users.noreply.github.com>
2024-08-23 10:35:50 -0400

    a

--- api.js
+++ api.js
@@ -2,22 +2,16 @@
 import { fatorialJS } from "./fat.js";
 
 async function main() {
-  const aOut = await import("./fatorial.out.js");
-  //const { resultado: resultadoAOut, tempoExecucao: tempoExecucaoAOut } = medirTempoDeExecucao(aOut.run);
+  const WASM = await import("./fatorial.out.js");
   console.time("tempoExecucao WASM");
 
-  // Código cuja execução você quer medir
-  for (let i = 0; i < 1000000; i++) {
-    aOut.run
-  }
+  WASM.run
 
   console.timeEnd("tempoExecucao WASM");
+
   console.time("tempoExecucao JS");
 
-  // Código cuja execução você quer medir
-  for (let i = 0; i < 10000n; i++) {
-    fatorialJS(100000)
-  }
+  fatorialJS(100000)
 
   console.timeEnd("tempoExecucao JS");
   
