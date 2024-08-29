eebeb8ef624bc10d8615aab006b543d1d097ba7b
Neto <97475687+neto2022@users.noreply.github.com>
2024-08-29 09:35:07 -0400

    a

---
+++ fatorial300.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <gmp.h>
+
+void fatorial(int n)
+{
+     for(long int fat = 1; n > 1; n = n - 1)
+  {      
+      fat = fat * n;
+  }
+}
+
+int main()
+{
+
+    for (int index = 0; index <= 300000; index++)
+    {
+        fatorial(index);
+        printf("fatorial C %d", index);
+        printf("\n");
+    }
+}
