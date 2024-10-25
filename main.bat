c3532b710928877d46db713087423206408964a9
Neto <97475687+neto2022@users.noreply.github.com>
2024-10-25 10:25:17 -0400

    a

--- main.bat
+++ main.bat
@@ -2,3 +2,5 @@ node .\wasm2x2.js
 node .\bench2x2.js
 node .\wasm5x5.js
 node .\bench5x5.js
+
+//emcc ..\pesquisa-webassembly\bench5x5.c -o bench5x5.js -s EXPORTED_FUNCTIONS='["_main", "_fatorial","_matriz_multiplicacao"]' && emcc ..\pesquisa-webassembly\bench2x2.c -o bench2x2.js -s EXPORTED_FUNCTIONS='["_main", "_fatorial","_matriz_multiplicacao"]'
